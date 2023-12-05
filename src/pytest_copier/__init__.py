3946b0506a890dd3b5642d84c968b182c6190eaa
Axel H. <noirbizarre@users.noreply.github.com>
2023-12-05 16:34:19 +0100

    build(python)!: drop support for Python 3.7 and add support for Python 3.12

--- src/pytest_copier/__init__.py
+++ src/pytest_copier/__init__.py
@@ -1,2 +1,18 @@
-from ._version import __version__  # noqa: F401
-from .plugin import CopierFixture  # noqa: F401
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+
+from .errors import CopierError, CopierTaskError
+from .plugin import CopierFixture
+
+
+def read_version() -> str:
+    try:
+        return version("pytest-copier")
+    except (PackageNotFoundError, ImportError):
+        version_file = Path(__file__).parent / "VERSION"
+        return version_file.read_text() if version_file.is_file() else "0.0.0.dev"
+
+
+__version__ = read_version()
